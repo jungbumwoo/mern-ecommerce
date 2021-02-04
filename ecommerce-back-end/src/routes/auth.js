3e3a8de04733122d6e7d73c4f32101f3e3895459
jungbumwoo <jungbumwoo@naver.com>
2021-02-04 15:11:43 +0900

    Api Request Validation (signIn, signUp

--- ecommerce-back-end/src/routes/auth.js
+++ ecommerce-back-end/src/routes/auth.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
 const { signup, signin, requireSignin } = require('../controller/auth');
+const { validateRequest, validateSigninRequest, isRequestValidated } = require('../validators/auth');
 const router = express.Router();
 
 
-
-router.post('/signup', signup);
-router.post('/signin', signin);
+ 
+router.post('/signup', validateRequest, isRequestValidated, signup);
+router.post('/signin', validateSigninRequest, isRequestValidated, signin);
  
 router.post('/profile', requireSignin, (req, res) => {
     res.status(200).json({ user: 'profile'})
 })
 
-module.exports = router;    
+module.exports = router;
