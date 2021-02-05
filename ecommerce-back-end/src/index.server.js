05895e7da9bc6e63990d36a598b52491af6e36af
jungbumwoo <jungbumwoo@naver.com>
2021-02-06 00:33:28 +0900

    Update Category Api (2)

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const env = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 
 //routes
 const authRoutes = require('./routes/auth');
@@ -26,7 +27,8 @@ mongoose.connect(
     console.log(' ✅ Database connected')
 });
 
-app.use(express.json()); 
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', authRoutes);
 app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
