239749d098ee469ad6942cf365b90c9e1dbf2bae
jungbumwoo <jungbumwoo@naver.com>
2021-02-05 15:42:19 +0900

    Add product to cart api(1) - 08

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -8,6 +8,7 @@ const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
 
 //environment variable or you can say constants
 env.config();
@@ -29,6 +30,7 @@ app.use('/api', authRoutes);
 app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
+app.use('/api', cartRoutes);
 
 app.listen(process.env.PORT, ()=> {
      console.log(` ✅ Server is running on localhost:${process.env.PORT}`)
