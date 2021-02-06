f0b1a461a655a6002a93c5492656e18eef339937
jungbum <jungbumwoo@naver.com>
2021-02-06 18:24:21 +0900

    Admin Login & Private Route(2) 2810

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css'
+import PrivateRoute from './components/HOC/privateRoute';
 import Home from './container/Home';
 import Signin from './container/Signin';
 import Signup from './container/Signup';
@@ -12,7 +13,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <PrivateRoute exact path="/" component={Home}/>
           <Route path="/signin" component={Signin}/>
           <Route path="/signup" component={Signup}/>
         </Switch>
