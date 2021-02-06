fa75b909c20808acfc8a4dbd48d9d7f2286b8fe7
jungbumwoo <jungbumwoo@naver.com>
2021-02-06 11:35:57 +0900

    Admin Redux Store Setup (2)

--- admin-app/src/actions/auth.action.js
+++ admin-app/src/actions/auth.action.js
@@ -1,6 +1,7 @@
 import { authConstants } from "./constants"
 
 export const login = (user) => {
+    console.log("action, login")
     return (dispatch) => {
         dispatch({ 
             type: authConstants.LOGIN_REQUEST,
--- admin-app/src/container/Signin/index.js
+++ admin-app/src/container/Signin/index.js
@@ -3,53 +3,56 @@ import Layout from '../../components/Layout'
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import Input from '../../components/UI/Input'
 import { login } from "../../actions";
+import { useDispatch } from 'react-redux';
 /**
 * @author
 * @function Signin
 **/
 
 const Signin = (props) => {
-  const userLogin = (e) => {
-      e.preventDefault();
-      const user = {
-          email: "jungbum@naver.com",
-          password: "341341"
-      }
-      login(user);
-  };
+    const dispatch = useDispatch();
+    console.log("Signin Button clicked")
+    const userLogin = (e) => {
+        e.preventDefault();
+        const user = {
+            email: "jungbum@naver.com",
+            password: "341341"
+        }
+        dispatch(login(user));
+    };
 
-  return(
-    <Layout>
-        <Container>
-            <Row style={{ marginTop: "3.5rem"}}>
-                <Col md={{span: 6, offset: 3}}>
-                    <Form onSubmit={() => {userLogin()}}>
-                        <Input 
-                            label="Email"
-                            placeholder="Email"
-                            value=""
-                            type="text"
-                            onChange={() => {
-                            }}
-                        />
+    return(
+        <Layout>
+            <Container>
+                <Row style={{ marginTop: "3.5rem"}}>
+                    <Col md={{span: 6, offset: 3}}>
+                        <Form onSubmit={() => {userLogin}}>
+                            <Input 
+                                label="Email"
+                                placeholder="Email"
+                                value=""
+                                type="text"
+                                onChange={() => {
+                                }}
+                            />
 
-                        <Input 
-                            label="Password"
-                            placeholder="Password"
-                            value=""
-                            type="password"
-                            onChange={() => {
-                            }}
-                        />
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>                
-                </Col>
-            </Row>
-            
-        </Container>
-    </Layout>
+                            <Input 
+                                label="Password"
+                                placeholder="Password"
+                                value=""
+                                type="password"
+                                onChange={() => {
+                                }}
+                            />
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>                
+                    </Col>
+                </Row>
+                
+            </Container>
+        </Layout>
    )
 
  }
