32c877afe052bcb4f9ed4a894dc05a4fad55ee30
jungbum <jungbumwoo@naver.com>
2021-02-07 18:37:58 +0900

    Signout Api & Admin Dashboard(1) 542

--- ecommerce-back-end/src/controller/admin/auth.js
+++ ecommerce-back-end/src/controller/admin/auth.js
@@ -46,6 +46,7 @@ exports.signin = (req, res) => {
             if (user.authenticate(req.body.password) && user.role === 'admin') {
                 const token = await jwt.sign({_id: user._id, role: user.role}, process.env.JWT_SECRET, { expiresIn: '1h'});
                 const { _id, firstName, lastName, email, role, fullName } = user;
+                res.cookie('token', token, { expiresIn: '1h' });
                 res.status(200).json({
                     token,
                     user: {
@@ -63,3 +64,9 @@ exports.signin = (req, res) => {
     })
 }
 
+exports.signout = (req, res) => {
+    res.clearCookie('token');
+    res.status(200).json({
+        message: 'Signout succesfully...'
+    })
+}
