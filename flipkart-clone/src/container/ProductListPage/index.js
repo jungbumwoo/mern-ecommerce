b53b0e9fc56e6c24874190b8ec2c78557e624b1f
jungbumwoo <jungbumwoo@naver.com>
2021-02-10 11:13:37 +0900

    fetch product api and render product list - 19 (2)

--- flipkart-clone/src/container/ProductListPage/index.js
+++ flipkart-clone/src/container/ProductListPage/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { getProductsBySlug } from '../../actions';
-import Layout from '../../components/Layout'
+import Layout from '../../components/Layout';
+import './style.css';
 
 /**
 * @author
@@ -18,7 +19,27 @@ const ProductListPage = (props) => {
     }, [])
     return(
         <Layout>
-            <p>Product list Page</p>
+            <div className="card">
+                <div className="cardHeader">
+                    <div>Samsung mobile under 10k</div>
+                    <button>View All</button>
+                </div>
+                <div>
+                    <div className="productContainer">
+                        <div className="productImgContainer">
+                            <img src="http://localhost:2000/public/WZpPQoK5I-52e27980185045.Y3JvcCwxOTk5LDE1NjQsMCwzOQ.png" alt=""/>
+                        </div>
+                        <div>
+                            <div>Samsumg 4gb phone</div>
+                            <div>
+                                <span>4.3</span>
+                                <span>3353</span>
+                            </div>
+                            <div>5000</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
