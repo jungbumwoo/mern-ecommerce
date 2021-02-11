cce7affe4e417182607de5638e7555db585b5ba1
jungbumwoo <jungbumwoo@naver.com>
2021-02-11 14:30:03 +0900

    Delete Categories 3705

--- ecommerce-back-end/src/controller/category.js
+++ ecommerce-back-end/src/controller/category.js
@@ -1,5 +1,6 @@
 const Category = require('../models/category');
 const slugify = require('slugify');
+const shortid = require('shortid');
 
 function createCategories(categories, parentId = null){
     const categoryList = [];
@@ -24,7 +25,7 @@ function createCategories(categories, parentId = null){
 exports.addCategory = (req, res) => {
     const categoryObj = {
         name: req.body.name,
-        slug: slugify(req.body.name)
+        slug: `${slugify(req.body.name)}=${shortid.generate()}`
     }
 
     if(req.file){
@@ -86,6 +87,16 @@ exports.updateCategories = async(req, res) => {
     res.status(200).json({body: req.body});
 }
 
-exports.deleteCategories = (req, res) => {
-    res.status(200).json({body:req.body})
+exports.deleteCategories = async (req, res) => {
+    const { ids } = req.body.payload;
+    const deletedCategories = [];
+    for(let i=0; i< ids.length; i++){
+        const deleteCategory = await Category.findOneAndDelete({ _id: ids[i]._id })
+        deletedCategories.push(deleteCategory);
+    }
+    if(deletedCategories.length == ids.length){
+        res.status(200).json({message: 'Categories removed'});
+    } else {
+        res.status(400).json({message: 'Something went wrong'});
+    }
 }
